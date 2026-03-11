04da305c76a93efc667e328ed935d36588e555c1
magedElshafey <magedelshafey98@gmail.com>
2026-03-11 14:24:58 +0200

    updates

---
+++ src/store/AuthProvider.tsx
@@ -0,0 +1,101 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useRef,
+} from "react";
+import { useLocation } from "react-router-dom";
+import { isProtectedRoutes } from "@/utils/isProtectedRoutes";
+import {
+  getUserFromAnyStorage,
+  saveUserToStorage,
+  removeUserFromStorage,
+  StorageType,
+} from "@/features/auth/utils";
+import type { User } from "@/features/auth/types/auth.types";
+interface AuthContextProps {
+  user: User | null;
+  login: (userData: User, rememberUser?: boolean) => void;
+  logout: () => void;
+  loading: boolean;
+  lastPublicPage: React.MutableRefObject<string>;
+  updateUser: (userData: Partial<User>) => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  // global states
+  const location = useLocation();
+
+  // local states
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const lastPublicPage = useRef("/");
+
+  // effects
+  useEffect(() => {
+    if (!isProtectedRoutes(location?.pathname)) {
+      lastPublicPage.current = location.pathname + location.search;
+    }
+  }, [location]);
+
+  useEffect(() => {
+    const user = getUserFromAnyStorage();
+    if (user) {
+      setUser(user);
+    }
+    setLoading(false);
+  }, []);
+
+  const updateUser = (userData: Partial<User>) => {
+    const newUser: User = {
+      ...user!,
+      ...userData,
+    };
+
+    setUser((old) =>
+      old
+        ? {
+            ...newUser,
+          }
+        : null,
+    );
+
+    const storage = localStorage.getItem("user")
+      ? localStorage
+      : sessionStorage;
+    storage.setItem("user", JSON.stringify(newUser));
+  };
+
+  const login = (userData: User, rememberUser: boolean = false) => {
+    const storageType = rememberUser ? StorageType.LOCAL : StorageType.SESSION;
+    saveUserToStorage(userData, storageType);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    removeUserFromStorage();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ loading, user, login, logout, lastPublicPage, updateUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("use auth must be work in auth provider");
+  }
+  return context;
+};
+export default AuthProvider;
